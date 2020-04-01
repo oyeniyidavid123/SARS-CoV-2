1f4755fc4a25965663ff23aeca50b04fbf372a52
Amos Fung <amosfung@amoss-mbp.home>
2020-03-31 20:31:33 -0700

    basic overlay with map

---
+++ js/map.js
@@ -0,0 +1,12 @@
+let map = L.map('geomap', {
+    center: [37.09, -95.713],
+    zoom: 13
+});
+
+L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_key
+}).addTo(map);
+
